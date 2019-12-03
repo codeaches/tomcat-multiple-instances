dc04c570768c8f5d515fe11883b3fb661edfb72c
codeaches <info001@codeaches.com>
2019-12-02 23:13:55 -0700

    Tomcat multiple instances configuration example [http://codeaches.com/]

--- instance-2/instance-2-shutdown.bat
+++ instance-2/instance-2-shutdown.bat
@@ -3,6 +3,4 @@
 set CATALINA_HOME=M:\tomcat-multiple-instances\apache-tomcat-9.0.29
 set CATALINA_BASE=M:\tomcat-multiple-instances\instance-2
 
-set TITLE=Codeaches Tomcat Instance 2
-
-call %CATALINA_HOME%\bin\startup.bat %TITLE%
+call %CATALINA_HOME%\bin\shutdown.bat
